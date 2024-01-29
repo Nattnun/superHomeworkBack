efcf8422d086ce7b1135a70a2c79f5ede1e4bfda
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-01-29 15:36:25 +0700

    finish backend

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config(); //load file js that has config method
+// console.log(process.env);
+const cors = require("cors");
+const express = require("express");
+const notFound = require("./middlewares/notFound");
+const errorMiddleware = require("./middlewares/error");
+const authRoute = require("./routes/auth-route");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoute);
+
+app.use(notFound);
+app.use(errorMiddleware); //err ต้องอยู่ตัวสุดท้าย
+
+let port = process.env.PORT;
+app.listen(port, () => {
+  console.log("server on", port);
+});
---
+++ prisma/seed.js
@@ -0,0 +1,45 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const bcrypt = require("bcryptjs");
+
+const hashPassword = bcrypt.hashSync("123456", 10);
+
+const teacherData = [
+  {
+    firstname: "Andy",
+    t_code: "t001",
+    password: hashPassword,
+    email: "andy@ggg.mail",
+  },
+  {
+    firstname: "Boby",
+    t_code: "t002",
+    password: hashPassword,
+    email: "boby@ggg.mail",
+  },
+  {
+    firstname: "Candy",
+    t_code: "t003",
+    password: hashPassword,
+    email: "candy@ggg.mail",
+  },
+  {
+    firstname: "Danny",
+    t_code: "t004",
+    password: hashPassword,
+    email: "danny@ggg.mail",
+  },
+];
+
+const subjectData = [
+  { title: "HTML", description: "Write web page" },
+  { title: "CSS", description: "Style web page" },
+  { title: "JS", description: "Dynamic web page" },
+];
+
+async function run() {
+  //   await prisma.teacher.createMany({ data: teacherData });
+  await prisma.subject.createMany({ data: subjectData });
+}
+
+run();
